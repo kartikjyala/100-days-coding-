256a56c9b9468f883f014b38b54acdb8cce9a49e
kartikjyala <kartikjyala12345@gmail.com>
2026-08-11 21:36:54 +0530

    Add program for sum, difference, product, and quotient
    Implement basic arithmetic operations on two input numbers.

---
+++ Day -1 /Q2.c
@@ -0,0 +1,27 @@
+//Write a program to input two numbers and display their sum, difference, product, and quotient.
+#include <stdio.h>
+
+int main(){
+int a, b;
+scanf ("%d %d",&a, &b);
+printf ("sum= %d\n" ,a+b);
+printf ("diffrence= %d\n" ,a -b);
+printf ("product= %d\n" ,a*b);
+printf ("quotient= %d\n" ,a/b);
+    return 0;
+}
+/*PS C:\Users\lenovo\OneDrive\Desktop\100 days codding> gcc day1q2.c 
+PS C:\Users\lenovo\OneDrive\Desktop\100 days codding> .\a.exe   10
+2
+sum= 12
+diffrence= 8
+product= 20
+quotient= 5
+PS C:\Users\lenovo\OneDrive\Desktop\100 days codding> gcc day1q2.c
+PS C:\Users\lenovo\OneDrive\Desktop\100 days codding> .\a.exe   
+7 
+3
+sum= 10
+diffrence= 4
+product= 21
+quotient= 2*/
